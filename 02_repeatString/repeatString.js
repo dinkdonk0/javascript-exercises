d9852cf7e108d633ee129e567c09e5b9d060dab9
dinkdonk0 <128898053+dinkdonk0@users.noreply.github.com>
2023-03-29 21:41:02 +0200

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
+
+const repeatString = function(word, multiply) {
+    if(multiply<1){
+        return "Error";
+    }
+    let newWord = "";
+    for(let i=0;i<multiply;i++){
+        newWord+=word;
+    }
+    return newWord;
 };
-
+repeatString("hey",3);
 // Do not edit below this line
 module.exports = repeatString;
